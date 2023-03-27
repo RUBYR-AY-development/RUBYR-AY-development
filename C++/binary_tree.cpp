1cab2bd5d811c8dafae71c40afba682f54dbb0a1
RUBYR_AY (RUBY ♕) <117422120+RUBYR-AY-development@users.noreply.github.com>
2023-03-26 23:37:01 -0700

    Create binary_tree.cpp

---
+++ C++/binary_tree.cpp
@@ -0,0 +1,47 @@
+/*
+* simple binary tree i made (for practice)
+* has infinite children and simple search func
+*/
+
+#include <iostream>
+#include <vector>
+
+template <typename T> struct node
+{
+    T data;
+    std::vector<node<T>*> children;
+
+    void add_child(T data)
+    {
+        node<T>* child = new node<T>(data);
+        this->children.push_back(child);
+    }
+
+    node(T data) { this->data = data; }
+
+    bool search(T request) // searches itself and children for it
+    {
+        if (this->data == request)
+            return true;
+        else
+            for (node<T>* x : children)
+                return x->search(request);
+    }
+};
+
+int main()
+{
+    node<int>* root = new node<int>(1);
+    root->add_child(2); // root->child1
+    root->children[0]->add_child(3); // root->child1->child1
+
+    std::cout << root->data << '\n' << root->children[0]->data << '\n';
+
+    bool success = root->search(4);
+    if (success)
+        std::cout << "hooray";
+    else
+        std::cout << "saddening";
+
+    return 0;
+}
